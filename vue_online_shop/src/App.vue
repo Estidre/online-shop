2e56b6faf67c57b4005054dd8394e3cc08b820da
Eric <kael.e@bk.ru>
2020-04-23 11:35:22 +0300

    upd9.1

--- vue_online_shop/src/App.vue
+++ vue_online_shop/src/App.vue
@@ -1,16 +1,41 @@
 <template>
   <div id="app">
     <main-wrapper />
+    <v-select 
+      :options="options"
+      @select="optionSelect"
+      :selected="selected"
+    />
+    <p>{{selected}}</p>
   </div>
 </template>
 
 <script>
 import MainWrapper from './containers/Main-wrapper.vue'
+import vSelect from './components/v-select.vue'
 
 export default {
   name: 'App',
   components:{
+    vSelect,
     MainWrapper
+  },
+  data(){
+    return{
+      options:[
+        {name:'Option 1', value: 1},
+        {name:'Option 2', value: 2},
+        {name:'Option 3', value: 3},
+        {name:'Option 4', value: 4},
+        {name:'Option 5', value: 5}
+      ],
+      selected: 'Select'
+    }
+  },
+  methods:{
+    optionSelect(option){
+      this.selected = option.name
+    }
   }
 }
 </script>
---
+++ vue_online_shop/src/components/v-select.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="select">
+        <p 
+        class="select__title" @click="areOptionsVisible = !areOptionsVisible"
+        >{{selected}}</p>
+        <div 
+        class="options"
+        v-if="areOptionsVisible"
+        >
+            <p
+                v-for="option in options"
+                :key="option.value"
+                @click="selectOption(option)"
+            >
+            {{option.name}}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'v-select',
+    props: {
+        options:{
+            type: Array,
+            default(){
+                return []
+            }
+        },
+        selected: {
+            type: String,
+            default: ''
+        }
+    },
+    dara(){
+        return{
+            areOptionsVisible: false
+        }
+    },
+    methods:{
+        selectOption(option){
+            this.$emit('select', option)
+            this.areOptionsVisible = false
+        },
+        hideSelect(){
+            this.areOptionsVisible = false
+        }
+    },
+    mounted(){
+        document.addEventListener('click', this.hideSelect.bind(this), true)
+    },
+    beforeDestroy(){
+        document.removeEventListener('click', this.hideSelect)
+    }
+}
+</script>
+
+<style lang="scss">
+    .select{
+        position: relative;
+        width: 200px;
+        cursor: pointer;
+        p{
+            margin: 0;
+        }
+        &__title{
+            border: solid 1px #aeaeae;
+        }
+    }
+    .options{
+        border: solid 1px #aeaeae;
+        position: absolute;
+        top: 30px;
+        right: 0;
+        width: 100%; 
+        p{
+            &:hover{
+                background: #e7e7e7;
+            }
+        } 
+    }
+</style>
