4a7c39f8b109baf014ccc4bb6952d7e8f6af2a46
Eric <kael.e@bk.ru>
2020-04-27 15:32:56 +0300

    upd11

--- vue_online_shop/src/containers/Catalog.vue
+++ vue_online_shop/src/containers/Catalog.vue
@@ -4,12 +4,37 @@
             <div class="catalog__link_to_cart">Cart: {{CART.length}}</div>
         </router-link>
         <h1>Catalog</h1>
-        <v-select
-        :selected="selected"
-        :options="categories"
-        @select="sortByCategories"
-        :isExpended="IS_DESKTOP"
-        />
+        <div class="filters">
+             <v-select
+            :selected="selected"
+            :options="categories"
+            @select="sortByCategories"
+            :isExpended="IS_DESKTOP"
+            />
+            <div class="range-slider">
+                <input 
+                type="range" 
+                min="0" 
+                max="1000" 
+                step="10"
+                v-model.number="minPrice"
+                @change="setRangeSlider"
+                >
+                <input 
+                type="range" 
+                min="0" 
+                max="1000" 
+                step="10"
+                v-model.number="maxPrice"
+                @change="setRangeSlider"
+                >
+            </div>
+            <div class="range-values">
+                <p>Min:{{minPrice}}</p>
+                <p>Max:{{maxPrice}}</p>
+            </div>
+        </div>
+       
         <div class="catalog-list">
             <item 
             v-for="product in filteredProducts" 
@@ -25,7 +50,6 @@
 import Item from '../components/Item.vue'
 import {mapActions, mapGetters} from 'vuex'
 import vSelect from '../components/v-select.vue'
-
 export default {
     name: 'Catalog',
     components:{
@@ -41,7 +65,9 @@ export default {
             {name:'Унисекс', value: 'n'}
             ],
             selected: 'Все',
-            sortedProducts: []
+            sortedProducts: [],
+            minPrice: 0,
+            maxPrice: 1000
         }
     },
     computed:{
@@ -66,19 +92,36 @@ export default {
         addToCart(data){
             this.ADD_TO_CART(data)
         },
+        setRangeSlider(){
+            if(this.minPrice > this.maxPrice){
+                let tmp = this.maxPrice
+                this.maxPrice = this.minPrice
+                this.minPrice = tmp
+            }
+            this.sortByCategories()
+        },
         sortByCategories(category){
-            this.sortedProducts = []
             let vm = this
-            this.PRODUCTS.map(function(item){
-                if(item.category === category.name){
-                    vm.sortedProducts.push(item)
-                }
+            this.sortedProducts = [...this.PRODUCTS]
+            this.sortedProducts = this.sortedProducts.filter(function(item){
+                return item.price >= vm.minPrice && item.price <= vm.maxPrice
             })
-            this.selected = category.name
-        }
+            if(category){
+            this.sortedProducts = this.sortedProducts.filter(function(item){
+                vm.selected === category.name
+                return item.category === category.name
+            })
+            }
+        },
     },
     mounted(){
         this.GET_PRODUCTS()
+        .then((res)=>{
+            if(res.data){
+                console.log('yes')
+                this.sortByCategories()
+            }
+        })
     }
 }
 </script>
@@ -97,6 +140,28 @@ export default {
             right: 10px;
             padding: 16px;
             border: solid 1px #aeaeae;
+        }
+    }
+    .filters{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .range-slider{
+        width: 200px;
+        margin: auto 16px;
+        text-align: center;
+        position: relative;
+       svg, input[type=range]{
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        }
+        input[type=range]::-webkit-slider-thumb{
+        z-index: 2;
+        position: relative;
+        top: 2px;
+        margin-top: -7px;
         }
     }
 </style>
